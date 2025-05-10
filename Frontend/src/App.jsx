97f9027659858298ef226008104418af8b5c0f03
sujal290 <117660164+sujal290@users.noreply.github.com>
2025-05-10 12:47:07 +0530

    verifed

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -14,7 +14,7 @@ import Footer from './components/Footer';
 import SearchBar from './components/SearchBar';
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import { Verify } from './Pages/Verify' 
+import  Verify  from './Pages/Verify' 
 
 
 
